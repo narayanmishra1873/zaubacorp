79c70c065a964879d888448ab6208baba101aa51
narayanmishra1873 <112859055+narayanmishra1873@users.noreply.github.com>
2025-05-15 12:21:50 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -121,4 +121,5 @@ async def extract_investor_section(company_name: str) -> str:
 if __name__ == "__main__":
     #print(asyncio.run(get_correct_company_name('CashRich is a Mutual fund company')))
     #mcp.run(transport = "streamable-http", host="0.0.0.0", port=8000)
-    mcp.run(transport="streamable-http")
+    port = int(os.environ.get("PORT", 8000)) 
+    mcp.run(transport="sse", port=port)
